35df68d3037a1685a0f3b3726f89bc3537bde61f
R. Nick McCandless <nick@mccandless-development.com>
2018-05-25 12:28:03 -0600

    Replaces org unit tests w/ rspec & factory bot

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'paperclip', '~> 6.0.0'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails', '~> 3.7'
+  gem 'factory_bot_rails'
+
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,19 @@ module InterviewTest
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    #
+
+    require "rails"
+    # Pick the frameworks you want:
+    require "active_model/railtie"
+    require "active_job/railtie"
+    require "active_record/railtie"
+    require "action_controller/railtie"
+    require "action_mailer/railtie"
+    require "action_view/railtie"
+    require "action_cable/engine"
+    require "sprockets/railtie"
+    # require "rails/test_unit/railtie"
+    config.generators.system_tests = nil
   end
 end
