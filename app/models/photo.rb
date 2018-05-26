0fb1eed888feeecb4cfc6d4af90b562e7dabe979
R. Nick McCandless <nick@mccandless-development.com>
2018-05-25 18:37:55 -0600

    Adds the rest of the relations to the models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
