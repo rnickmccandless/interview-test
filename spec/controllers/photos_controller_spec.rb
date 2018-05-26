b85e656eb2c84650aefd1005caab9a925ced582a
R. Nick McCandless <nick@mccandless-development.com>
2018-05-26 00:45:23 -0600

    Adds edit action to photo & controller specs

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PhotosController, type: :controller do
+  describe "GET #create" do
+    it "returns http success" do
+      photo_params = attributes_for :photo
+      photo_params[:user_id] = create(:user).id
 
+      expect { post :create, params: { photo: photo_params } }.to change(Photo, :count).by(1)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      photo = create :photo
+
+      get :edit, params: { id: photo.id }
+
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
--- spec/factories/photo.rb
+++ spec/factories/photo.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :photo do
-    image { File.new("#{Rails.root}/spec/support/fixtures/test.jpg") }
+    image { Rack::Test::UploadedFile.new('spec/support/fixtures/test.jpg', 'image/png') }
     caption <<~HEREDOC
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt a neque non sagittis.
     HEREDOC
